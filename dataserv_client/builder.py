cf7af7946181a095e8975992cee34427d5432c2a
Shawn Wilkinson <me@super3.org>
2015-08-04 15:59:38 -0400

    Code Update an Seed File Name

--- dataserv_client/builder.py
+++ dataserv_client/builder.py
@@ -4,7 +4,6 @@ import RandomIO
 
 
 class Builder:
-
     def __init__(self, address, shard_size, max_size):
         self.address = address
         self.shard_size = shard_size
@@ -25,30 +24,18 @@ class Builder:
 
     def generate_shard(self, seed, store_path, cleanup=False):
         """Save a shard, and return its SHA-256 hash."""
-
-        # temporarily generate file
-        tmp_file = RandomIO.RandomIO(seed).read(self.shard_size)
-
+        tmp_file = RandomIO.RandomIO(seed).read(self.shard_size)  # temporarily generate file
         file_hash = hashlib.sha256(tmp_file).hexdigest()  # get SHA-256 hash
-
-        # save the shard
-        path = os.path.join(store_path, file_hash)
-        RandomIO.RandomIO(seed).genfile(self.shard_size, path)
+        RandomIO.RandomIO(seed).genfile(self.shard_size, store_path+seed)  # save the shard
         if cleanup:
-            os.remove(path)
+            os.remove(store_path+seed)
         return file_hash
 
     def build(self, store_path, debug=False, cleanup=False):
         """Fill the farmer with data up to their max."""
-        hashes = []
         for shard_num in range(int(self.max_size/self.shard_size)):
             seed = self.build_seed(shard_num)
             file_hash = self.generate_shard(seed, store_path, cleanup)
-            hashes.append(file_hash)
 
             if debug:
-                print("Saving seed {0} with SHA-256 hash {1}.".format(
-                    seed, file_hash
-                ))
-
-        return hashes
+                print("Saving seed {0} with SHA-256 hash {1}.".format(seed, file_hash))
